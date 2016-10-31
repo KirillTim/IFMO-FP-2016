41b444c5efd4b9211cac71de4c9719ca97762bb4
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2016-10-31 18:20:05 +0300

    sexpr

---
+++ hw5/SExpr.hs
@@ -0,0 +1,47 @@
+module SExpr where
+
+import           AParser
+import           Control.Applicative
+import           Control.Monad       (void)
+import qualified Data.Char           as C
+
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore = many
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore = some
+
+spaces :: Parser String
+spaces = zeroOrMore (satisfy C.isSpace)
+
+ident :: Parser String
+ident = (++) <$> oneOrMore (satisfy C.isAlpha) <*> zeroOrMore (satisfy C.isAlphaNum)
+
+type Ident = String
+
+data Atom = N Integer | I Ident
+  deriving Show
+
+data SExpr = A Atom
+           | Comb [SExpr]
+  deriving Show
+
+parseAtom :: Parser Atom
+parseAtom = spaces *> ((I <$> ident) <|> (N <$> posInt)) <* spaces
+
+openB :: Parser ()
+openB = void(char '(') <* spaces
+
+closeB :: Parser ()
+closeB = void spaces <* char ')'
+
+parseSExpr :: Parser SExpr
+parseSExpr =
+  (A <$> parseAtom) <|> (openB *> (Comb <$> oneOrMore parseSExpr) <* closeB)
+
+-- "5"
+-- "foo5"
+-- (bar (foo) 3 5 874)
+-- (((lambda x (lambda y (plus x y))) 3)
+-- (   lots  of   (  spaces   in  )  this ( one ) )
