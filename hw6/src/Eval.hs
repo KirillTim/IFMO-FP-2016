26d748aaf176eeda46766cf95614701480188917
Kirill Timofeev <timofeev.kirill.a@gmail.com>
2016-11-07 18:01:59 +0300

    task 2 done

--- hw6/src/Eval.hs
+++ hw6/src/Eval.hs
@@ -1,5 +1,7 @@
-import qualified Data.Map as M
-import Control.Monad.Reader
+import           Control.Applicative  ((<$>))
+import           Control.Monad        (liftM2)
+import           Control.Monad.Reader (Reader, ask, local, runReader)
+import qualified Data.Map             as M (Map, insert, empty, lookup)
 data Expr = Const Integer
   | Var String
   | Sum Expr Expr
@@ -8,8 +10,16 @@ data Expr = Const Integer
   deriving (Show)
 
 eval :: Expr -> Reader (M.Map String Integer) (Maybe Integer)
-eval (Const n) = return $ Just n
-eval (Var v)   = ask >>= lift . M.lookup v --return $ maybe Nothing id (M.lookup v ask)
---eval (Sum left right) = liftM2 (+) (eval left) (eval right)
-eval (Sum x y) =  eval x >>= (<$> eval y) . liftM2 (+)
---eval (Assign name val expr) = return $ local (M.insert name val) (eval expr)
+eval (Const n)              = return $ Just n
+eval (Var v)                = ask >>= \env -> return $ M.lookup v env
+eval (Sum x y)              =  eval x >>= (<$> eval y) . liftM2 (+)
+eval (Mul x y)              =  eval x >>= (<$> eval y) . liftM2 (*)
+eval (Assign name val expr) = local (M.insert name val) (eval expr)
+
+expr1 = Const 10 `Sum` ("x" `Assign` 2 $ Var "x")
+--expr2 = "x" `Assign` 2 $ (Var "x" `Mul` ("y" `Assign` 10 $ Var "y"))
+--expr3 = Var "x" `Mul` Const 10
+
+--runReader (eval expr1) M.empty
+main :: IO ()
+main = do putStrLn . show $ runReader (eval expr1) M.empty
